a2a6d7fe00dd89950af0b4b402d99623a49e7500
lalitsom <lalitsom27@gmail.com>
2019-09-23 20:23:11 +0530

    course project no btn margin fix

--- recepie-app/src/app/shopping/item-list/item-list.component.ts
+++ recepie-app/src/app/shopping/item-list/item-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Item } from '../../shared/item.model';
 
 @Component({
   selector: 'app-item-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item-list.component.css']
 })
 export class ItemListComponent implements OnInit {
+  itemList: Item[] = [
+    new Item("dhaniya","50"),
+    new Item("podhina","50")
+  ];
+
   constructor() { }
 
   onCreateItem(){
